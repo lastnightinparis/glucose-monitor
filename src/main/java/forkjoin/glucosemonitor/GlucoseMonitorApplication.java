3807561c05e816c34afcfddd6b4ba70066d45d68
Kirill Tankovskiy <tankovskiyko@gmail.com>
2022-12-17 14:18:45 +0300

    Initial commit

---
+++ src/main/java/forkjoin/glucosemonitor/GlucoseMonitorApplication.java
@@ -0,0 +1,13 @@
+package forkjoin.glucosemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlucoseMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlucoseMonitorApplication.class, args);
+    }
+
+}
